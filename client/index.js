4dbf94397cb4270832818501d06c9623c301a46e
greatmaxix <greatmaxix@gmail.com>
2021-07-18 18:18:23 +0600

    hotfix

--- client/index.js
+++ client/index.js
@@ -1,7 +1,4 @@
-import Helper from './src/services/helper';
 import mainPageState from './src/services/mainPageState';
-import TestAppApiService from './src/services/testAppApi';
-import TestState from './src/services/testState';
 
 let pageState = new mainPageState();
 pageState.startTests();
